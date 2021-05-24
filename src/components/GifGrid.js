dd0992d768ca7b3fe86bc03610adf02ed0bdacc2
Eliu Hernandez <3liuhernandez@gmail.com>
2021-05-24 16:52:25 -0400

    a

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -6,7 +6,7 @@ import MetaTags from 'react-meta-tags';
 
 import {Helmet} from "react-helmet";
 
-import image__ from '../images/balanz.jpg';
+import image__ from './balanz.jpg';
 export const GifGrid = ({ category }) => {
 
     const { data:gifs, loading } = useFetchGifs( category );
@@ -40,29 +40,21 @@ export const GifGrid = ({ category }) => {
             </MetaTags> */}
 
             <Helmet>
-            <title>My METADATOS AAAA</title>
+                {/* <meta charSet="utf-8" /> */}
+                <title>Buscamos Gente</title>
+                {/* <meta name="description" content="Helmet application" /> */}
+                <meta property="og:url" content="https://rrhh.balanz.com/aplicar/33" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Buscamos Desarrollador iOS" />
+                <meta property="og:description" content="Entre sus principales responsabilidades se" />
+                <meta property="og:image" content={image__} />
+                
+                <link rel="canonical" href="http://mysite.com/example" />
 
-            <meta name="description" content="Otra web de desarrollo web AAAA" />
-
-            <link rel="canonical" href="http://oropensando.com/" />
-
-            {/* Open Graph para Facebook */}
-            <meta property="og:title" content="OROPEnSAndo" /> 
-            <meta property="og:type" content="website" /> 
-            <meta property="og:url" content="http://oropensando.com/" />
-            <meta property="og:image" content={image__}/>
-            <meta property="og:description" content="Otra web de desarrollo web" /> 
-            <meta property="og:site_name" content="Oropensando" />
-            <meta property="og:locale" content="es_ES" />
-            <meta property="fb:admins" content="Facebook ID" />
-
-            {/* Twitter Card */}
-            <meta name="twitter:card" content="summary_large_image"/> 
-            <meta name="twitter:site" content="@oropensando" /> 
-            <meta name="twitter:title" content="OROPEnSAndo" /> 
-            <meta name="twitter:description" content="Otra web de desarrollo web" /> 
-            <meta name="twitter:creator" content="@oropensando" /> 
-            <meta name="twitter:image:src" content={image__} />
+                {/* <meta property="og:title" content={'Hola'} />
+                <meta property="og:type" content={'website'} />
+                <meta property="og:image" content="{Your content}" />
+                <meta property="og:url" content="{Your content}" /> */}
             </Helmet>
 
 
