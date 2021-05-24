0998100d141ea583f3a0d0936338209d36d239cd
Eliu Hernandez <3liuhernandez@gmail.com>
2021-05-24 11:00:22 -0400

    fix babel/core and try to see meta tags

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,8 @@
 import { GifGridItem } from "./GifGridItem"; 
 
-import { useFetchGifs } from "../hooks/useFetchGifs"
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
+import MetaTags from 'react-meta-tags';
 
 export const GifGrid = ({ category }) => {
 
@@ -9,6 +10,16 @@ export const GifGrid = ({ category }) => {
 
     return (
         <>
+            <MetaTags>
+                <title>Page 12</title>
+                <meta name="description" content="Some description." />
+                <meta property="og:url" content="produccion.url.com" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content={`Buscamos hacer el meta`} />
+                <meta property="og:description" content="implementando los metas" />
+                <meta property="og:image" content={`src/images/balanz.jpg`} />
+            </MetaTags>
+
             <h3 className="animate__animated animate__backInLeft"> { category } </h3>
 
             { loading && <p className="animate__animated animate__flash animate__infinite">Loading</p>}
