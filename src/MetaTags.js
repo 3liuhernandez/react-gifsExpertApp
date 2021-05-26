24bb6acd27be37b6bcce0d62a864810250e851aa
Eliu Hernandez <3liuhernandez@gmail.com>
2021-05-26 10:11:36 -0400

    verra

--- src/MetaTags.js
+++ src/MetaTags.js
@@ -5,7 +5,7 @@ const getDefault = {
     title: "title default", 
     description: "My default Website description",
     keywords: "My default Website keyowrods",
-    robots:"follow",
+    robots:"index/follow",
     imagen: imagen__,
     canonicalUrl: 'https://gomuf.com'
 };
@@ -37,13 +37,6 @@ const MetaTags = () => {
     const meta = getPageMetadata();
     return (
         <Helmet>
-            <title>{meta.title}</title>
-            <meta name="description" content={meta.description} />
-            <meta name="keywords" content={meta.keywords} />
-            {meta.robots && <meta name="robots" content={meta.robots} />}
-            <link rel="canonical" href={meta.canonicalUrl} />
-            <meta name="imagen" content={meta.imagen} />
-
             <meta charset="utf-8" />
             <link rel="icon" href="./favicon.ico" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -52,6 +45,21 @@ const MetaTags = () => {
             <link rel="manifest" href="./manifest.json" />
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
             <link rel="stylesheet" href="./index.css"/>
+            <title>{meta.title}</title>
+            <meta name="description" content={meta.description} />
+            <meta name="keywords" content={meta.keywords} />
+            {meta.robots && <meta name="robots" content={meta.robots} />}
+            <link rel="canonical" href={meta.canonicalUrl} />
+            <meta name="imagen" content={meta.imagen} />
+
+            <mata property="og:title" content="Factores de posicionamiento de Google: 10 herramientas que te ayudarán"/>
+            <meta property="og.type" content="website"/>
+            <meta property="og:description" content="aqui va el contenido"/>
+            <meta property="og:image" content="https://3liuhernandez.github.io/react-gifsExpertApp/images/balanz.jpg" />
+            <meta property="og:url" content="https://gomuf.com" />
+            <meta property="og:site_name" content="https://gomuf.com" />
+
+            
 
         </Helmet>
     );
